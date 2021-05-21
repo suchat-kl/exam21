53d6b04bf78c650067a3246912fc258a7014508a
suchat <suchat.msit04@gmail.com>
2021-05-21 13:24:57 +0700

    complete ex21

---
+++ src/Exam21.java
@@ -0,0 +1,24 @@
+
+public class Exam21 {
+
+	public static void main(String[] args) {
+		// test commnet Single Line and Multiple Line
+		// this is comment Single Line
+		/*
+		 * this comment multiple Line
+		 */
+		int age = 50;
+		boolean male = true;
+		String txt = "ThaiLand";
+		double sal = 500.25D;
+		float score = 50.6F;
+
+		System.out.println("This is a Age " + age);
+		System.out.println("This is a Male " + male);
+		System.out.println("This is a Text " + txt);
+		System.out.println("This is a Salary " + sal);
+		System.out.println("This is a Score " + score);
+
+	}
+
+}
